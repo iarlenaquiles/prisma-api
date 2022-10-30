c9ad25799b7eaff99224ae59af06e1d9d5f55812
Iarlen Aquiles <iarlensilva775@gmail.com>
2022-10-30 18:55:08 -0300

    added interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor'
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
@@ -11,10 +13,13 @@ async function bootstrap() {
             whitelist: true,
             forbidNonWhitelisted: true,
             transform: true,
-        })
+        }),
     )
 
     app.useGlobalFilters(new HttpExceptionFilter())
+
+    app.useGlobalInterceptors(new UnauthorizedInterceptor())
+    app.useGlobalInterceptors(new NotFoundInterceptor())
     await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
