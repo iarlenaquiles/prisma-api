e6d7f2d6c2ef3cf500276e9ff16bfce91511e293
Iarlen Aquiles <iarlensilva775@gmail.com>
2022-10-31 20:27:11 -0300

    fix(): added post resource

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,29 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { UsersRepository } from './repositories/users.repository';
+import { Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { UsersRepository } from './repositories/users.repository'
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly repository: UsersRepository) {}
+    constructor(private readonly repository: UsersRepository) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.repository.create(createUserDto);
-  }
+    create(createUserDto: CreateUserDto) {
+        return this.repository.create(createUserDto)
+    }
 
-  findAll() {
-    return this.repository.findAll();
-  }
+    findAll() {
+        return this.repository.findAll()
+    }
 
-  findOne(id: number) {
-    return this.repository.findOne(id);
-  }
+    findOne(id: number) {
+        return this.repository.findOne(id)
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return this.repository.update(id, updateUserDto);
-  }
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return this.repository.update(id, updateUserDto)
+    }
 
-  remove(id: number) {
-    return this.repository.remove(id);
-  }
+    remove(id: number) {
+        return this.repository.remove(id)
+    }
 }
