8317b3b830f85e93875314cd032cea1f514433f3
Iarlen Aquiles <iarlensilva775@gmail.com>
2022-11-01 21:28:56 -0300

    added swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,14 @@
-import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class CreateUserDto {
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-  @IsBoolean()
-  admin: boolean;
+    @ApiProperty({ description: 'Email do usuário' })
+    @IsEmail()
+    @IsNotEmpty()
+    email: string
+    @IsString()
+    @IsNotEmpty()
+    name: string
+    @IsBoolean()
+    admin: boolean
 }
